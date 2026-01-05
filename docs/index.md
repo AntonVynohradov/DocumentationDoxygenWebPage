a24bd40bc76733f58d11120373705a3a6bdce477
Anton Vynohradov <vynohradov.external@bavariamatic.com>
2026-01-05 19:24:00 +0100

    Add init commit

---
+++ docs/index.md
@@ -0,0 +1,7 @@
+# MyProject
+
+To jest przykładowa dokumentacja.
+
+- pisana w Markdown
+- hostowana przez MkDocs
+- API generowane z Doxygen
---
+++ include/math.h
@@ -0,0 +1,11 @@
+/// ## MathUtils
+/// Proste funkcje matematyczne
+class MathUtils {
+public:
+    /// Dodaje dwie liczby
+    /// 
+    /// @param a pierwsza liczba
+    /// @param b druga liczba
+    /// @return suma
+    static int add(int a, int b);
+};
